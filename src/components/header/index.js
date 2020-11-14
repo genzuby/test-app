9a1fc2a128365055c9d32d94d78fb46215d579c4
genzuby <genzuby@gmail.com>
2020-11-14 20:39:59 +0100

    header test

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -4,10 +4,10 @@ import Logo from '../../assets/img/smile.png';
 
 function Header() {
   return (
-    <header>
+    <header className="headerComponent" data-test="headerComponent">
       <div className="wrap">
         <div className="logo">
-          <img src={Logo} alt="logo" />
+          <img src={Logo} className="logoimg" alt="logo" data-test="logoimg" />
         </div>
       </div>
     </header>
